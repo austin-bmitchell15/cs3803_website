204e8234fc32feadc4b7e3d20d3e0eeb1e5414bf
Rohan Mehta <rmehta98@gatech.edu>
2024-04-22 17:07:01 -0400

    fix pivot tables stuff

--- src/services/ModuleStatusStorage.ts
+++ src/services/ModuleStatusStorage.ts
@@ -140,7 +140,7 @@ export const tableOfContentsInit = [
             {
                 "name": "Pivot Tables",
                 "id": "pandas_pivottables",
-                "path": "/pandas/pivottables",
+                "path": "/pandas/pivottable",
                 "checked": false
             },
             {
